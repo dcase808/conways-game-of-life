1507598d81949ff0886d025522d4f70f2d11d00c
case <56609440+dcase808@users.noreply.github.com>
2021-12-23 21:51:33 +0100

    Alive and iteration counter
    Added alive pixels, and current iteration counter

--- main.py
+++ main.py
@@ -6,10 +6,8 @@ def init_random():
     board = np.random.randint(2, size=(resolution[0] // scale, resolution[1] // scale))
     for i, rows in enumerate(board):
         for j, columns in enumerate(rows):
-
             if i == 0 or j == 0 or i == game[0] - 1 or j == game[1] - 1:
                 board[i][j] = 0
-                print(f"set i, j: {i}, {j}, to 0")
     return board
 
 
@@ -46,9 +44,13 @@ scale = 5
 game = (resolution[0] // scale, resolution[1] // scale)
 
 pg.init()
+font = pg.font.SysFont(None, 48)
+img = font.render(f"Alive pixels: {scale}", True, (255, 255, 255))
+
 windows = pg.display.set_mode(resolution)
 pixels = init_random()
 # pixels = init_glider()
+iteration = 0
 
 running = True
 
@@ -56,8 +58,13 @@ while running:
     pixels = update(pixels)
     surface = pg.surfarray.make_surface(pixels * 253)
     surface = pg.transform.scale(surface, (resolution[0], resolution[1]))
+    img = font.render(f"Alive pixels: {np.sum(pixels)}", True, (255, 255, 255))
+    img2 = font.render(f"Iteration: {iteration}", True, (255, 255, 255))
     windows.blit(surface, (0, 0))
+    windows.blit(img, (20, 20))
+    windows.blit(img2, (20, 60))
     pg.display.update()
+    iteration += 1
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
